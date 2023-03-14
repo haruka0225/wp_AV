3d5612b94f02c1d1047bc1bbfd0258bb945a34a3
ishii yuka <xx2s2xx2@gmail.com>
2023-03-14 17:37:49 +0900

    footer.php追加

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <p>Copyright <small>&copy;</small> Shinohara.ALL RIGHTS RESERVED.</p>
+</footer>
+<!-- body終了タグの直前 -->
+<?php wp_footer(); ?> 
+<script src="https://code.jquery.com/jquery-3.6.3.min.js"
+    integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+  <script src="./script.js"></script>
+</body>
+</html>
