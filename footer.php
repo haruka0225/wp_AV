3b4f61c20a3b3f9a42596fd0ec22bac51465bb97
ishii yuka <xx2s2xx2@gmail.com>
2023-03-16 19:10:19 +0900

    フッター編集

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </footer>
 <!-- body終了タグの直前 -->
 <?php wp_footer(); ?> 
-<script src="https://code.jquery.com/jquery-3.6.3.min.js"
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js"
     integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
   <script src="<?php echo get_template_directory_uri();?>/script.js"></script>
 </body>
