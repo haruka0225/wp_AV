8b216828fc2cd5404cfade5f6a40b16008ddf104
ishii yuka <xx2s2xx2@gmail.com>
2023-03-17 19:23:08 +0900

    もっと見るボタン追加

--- index.php
+++ index.php
@@ -17,22 +17,26 @@
 
 
   <!----- Latest Articles --------------->
-    <section class="Latest">
+    <section class="Latest-Articles">
       <h1>Latest Articles</h1>
-        <div class="Latest-box">
+        <div class="Latest">
           <!-- メインループここから -->
           <?php
             if (have_posts()) :
               while(have_posts()):
                 the_post();
           ?>
-          <div class="box">
-            <img class="Latest-img" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="post-img">
-              <div class="box-text">
-                <p><?php the_date(); ?></p>
-                <p><?php the_title(); ?></p>
-              </div>
-              <a href="#">READ MORE</a>
+          <div class="Latest-box">
+            <ul>
+              <li>
+                <img class="Latest-img" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="post-img">
+                  <div class="box-text">
+                    <p><?php the_date(); ?></p>
+                    <p><?php the_title(); ?></p>
+                  </div>
+                  <a href="#">READ MORE</a>
+              </li>
+            </ul>
           </div>
           <?php
             endwhile;
@@ -44,6 +48,7 @@
           <?php endif; ?>
           <!-- メインループここまで -->
         </div>
+        <button class="more-btn">もっと見る</button>
     </section>
   </main>
 <!-- フッター読み込み -->
