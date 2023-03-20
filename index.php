d79e54af042cef730c2db7f02480ed8a62c5eb7b
DESKTOP-J3PPFPG\harux <haru.xoxo7@gmail.com>
2023-03-20 13:27:00 +0900

    プラグインなおった

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="" class="logo1">
 
       <div class="img-box">
-      <!-- <?php //echo do_shortcode('[metaslider id="14"]'); ?>  -->
+      <?php echo do_shortcode('[metaslider id="14"]'); ?> 
       <!-- <img src="<?php //echo get_template_directory_uri(); ?>/assets/img/visual_1_pc.png" alt="" class="pic">
       <img src="<?php //echo get_template_directory_uri(); ?>/assets/img/visual_2_pc.png" alt="" class="pic">
       <img src="<?php //echo get_template_directory_uri(); ?>/assets/img/visual_3_pc.png" alt="" class="pic">   -->
